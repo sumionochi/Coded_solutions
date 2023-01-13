60b94704af717dde7822eb411252b5b513df9314
Aaditya Srivastava <89721628+sumionochi@users.noreply.github.com>
2023-01-13 18:13:13 +0530

    Time: 98 ms (99.05%), Space: 67.9 MB (13.77%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,17 +1,13 @@
-//kadane algo
 class Solution {
 public:
     int maxSubArray(vector<int>& a) {
-        int sum=0,maxsum=INT_MIN;
+        //original kadane algo
+        int localmax=0;
+        int globalmax=INT_MIN;
         for(int i=0;i<a.size();i++){
-            sum+=a[i];
-            if(sum>maxsum){
-                maxsum=sum;
-            }
-            if(sum<0){
-                sum=0;
-            }
+            localmax = max(a[i],a[i]+localmax);
+            globalmax = max(globalmax,localmax);
         }
-        return maxsum;
+        return globalmax;
     }
 };
