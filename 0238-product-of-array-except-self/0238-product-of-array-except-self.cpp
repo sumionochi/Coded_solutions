4fc797ca1a7b56e88c6204bb72d177615fa46601
Aaditya Srivastava <89721628+sumionochi@users.noreply.github.com>
2023-01-17 08:19:44 +0530

    Time: 38 ms (40.96%), Space: 25 MB (32.99%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& a) {
-        vector<int> l;
-        int save=1;
-        for(int i=0;i<a.size();i++){
-            save*=a[i];
-            l.push_back(save);
-        }
-
-        vector<int> r;
-        save=1;
-        for(int i=a.size()-1;i>=0;i--){
-            save*=a[i];
-            r.push_back(save);
-        }
-        reverse(r.begin(),r.end());
-
         vector<int> ans;
+        int ansit=1;
         for(int i=0;i<a.size();i++){
-            if(i==0) {ans.emplace_back(r[i+1]);continue;}
-            if(i==a.size()-1) {ans.emplace_back(l[i-1]);continue;}
-            ans.emplace_back(l[i-1]*r[i+1]);
+            ansit*=a[i];
+            ans.emplace_back(ansit);
+        }
+        int prod=1;
+        for(int i=a.size()-1;i>=0;i--){
+            if(i==a.size()-1){ans[i]=ans[i-1];prod*=a[i];continue;}
+            if(i==0){ans[i]=prod;continue;}
+            ans[i]=ans[i-1]*prod;
+            prod*=a[i];
         }
         return ans;
-    }   
+    }
 };
