f02cd0ef597ad96543f2c27e88beaf3b42058d75
Aaditya Srivastava <89721628+sumionochi@users.noreply.github.com>
2023-01-24 10:32:43 +0530

    Time: 8 ms (79.72%), Space: 13.6 MB (87.12%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -5,12 +5,9 @@ public:
         int l=0,h=a.size()-1;
         vector<int> ans;
 
+        //find first position
         while(l<=h){
             int m=l+(h-l)/2;
-            if(l==h){
-                if(a[l]==t) f=m;
-                break;
-            }
             if(a[m]==t){
                 f=m;
                 h=m-1;
@@ -18,19 +15,19 @@ public:
             if(a[m]<t) l=m+1;
             if(a[m]>t)h=m-1;
         }
+        
+        //when target is not present
         if(f==INT_MIN){
             ans.emplace_back(-1);
             ans.emplace_back(-1);
             return ans;
         }
         ans.emplace_back(f);
+
+        //find end position
         l=0,h=a.size()-1;
         while(l<=h){
             int m=l+(h-l)/2;
-            if(l==h){
-                if(a[l]==t) e=m;
-                break;
-            }
             if(a[m]==t){
                 e=m;
                 l=m+1;
